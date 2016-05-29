9248e90534efe1a1d18702f5867ba0da62474882
Benea Leonid <www.leo_md@mail.ru>
2016-05-29 13:14:44 +0300

    lab 5

---
+++ Lab 5/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+
+import info.fandroid.drop.Drop;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Game";
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new Drop(), config);
+	}
+}
